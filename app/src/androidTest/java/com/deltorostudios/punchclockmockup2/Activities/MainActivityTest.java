b90dd7ed8e28771d6566f2c0b2ea3dab385f2c18
Francis <francisglacken@gmail.com>
2019-04-09 02:15:31 -0400

    Messed around with testing

--- app/src/androidTest/java/com/deltorostudios/punchclockmockup2/Activities/MainActivityTest.java
+++ app/src/androidTest/java/com/deltorostudios/punchclockmockup2/Activities/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.deltorostudios.punchclockmockup2.Activities;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
 import com.deltorostudios.punchclockmockup2.R;
@@ -8,10 +9,17 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 public class MainActivityTest {
@@ -27,5 +35,26 @@ public class MainActivityTest {
     @Test
     public void DoViewsExistTest() {
         onView(withId(R.id.testView)).perform(replaceText("GET MONEY"));
+        onView(withId(R.id.addButton)).perform(click());
+
+        pressBack();
+        onView(withId(R.id.mainRecyclerView)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void CanWeGetItToSwipeToSecondTab() {
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.viewPager)).perform(swipeRight());
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.viewPager)).perform(swipeRight());
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.viewPager)).perform(swipeRight());
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.viewPager)).perform(swipeRight());
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.viewPager)).perform(swipeRight());
+        onView(withId(R.id.viewPager)).perform(swipeLeft());
+        onView(withId(R.id.viewPager)).perform(swipeRight());
     }
 }
